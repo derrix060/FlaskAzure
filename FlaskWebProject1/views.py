74a99105fd99ef570bcbc078efbd2457674b7ff1
Mario Apra <mario_surfing@hotmail.com>
2017-05-29 20:04:29 -0300

    add create event and item

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -3,18 +3,24 @@ Routes and views for the flask application.
 """
 
 from FlaskWebProject1 import app
+from flask import jsonify
+import json
 
 events_path = './FlaskWebProject1/sources/events.json'
 itens_path = './FlaskWebProject1/sources/itens.json'
 
 f = open(events_path, 'r')
-events = []
-events = f.read()
+events_file = []
+events_file = f.read()
+events = json.loads(events_file)
 f.close()
 f = open(itens_path, 'r')
-itens = []
-itens = f.read()
+itens_file = []
+itens_file = f.read()
+itens = json.loads(itens_file)
 f.close()
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -28,7 +34,7 @@ def teste():
 
 @app.route('/api/herdeiros/eventos/all')
 def get_all_events():
-    return events
+    return jsonify(events)
 
 
 @app.route('/api/herdeiros/itens/all')
@@ -36,17 +42,27 @@ def get_all_itens():
     return jsonify(itens)
 
 
-@app.route('/create_events')
-def create_events():
-    eventos = []
-    evento = {}
-    evento['name'] = 'teste name'
-    evento['date'] = "2017-06-05 17:00:00"
-    eventos.append(evento)
-    eventos.append(evento)
-    eventos.append(evento)
-    rtn = jsonify(eventos)
-    f = open(events_path, 'w+')
-    f.write(rtn)
+@app.route('/api/herdeiros/item/create/<name>/<description>')
+def create_events(name, description):
+    item = {}
+    item['name'] = name
+    item['description'] = description
+    item_j = json.dumps(item)
+    itens.append(item_j)
+    f = open(itens_path, 'w+')
+    f.write(itens)
     f.close()
-    return rtn
+    return 'Event created with success!'
+
+
+@app.route('/api/herdeiros/eventos/create/<name>/<date>')
+def create_events(name, date):
+    evento = {}
+    evento['name'] = name
+    evento['date'] = date
+    evento_j = json.dumps(evento)
+    events.append(evento_j)
+    f = open(events_path, 'w+')
+    f.write(events)
+    f.close()
+    return 'Event created with success!'
