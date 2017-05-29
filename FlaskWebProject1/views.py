6428a157db69567984a6619ec2e685ae2016dd58
Mario Apra <mario_surfing@hotmail.com>
2017-05-29 20:54:13 -0300

    fix add itens and add events

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -9,30 +9,30 @@ import json
 events_path = './FlaskWebProject1/sources/events.json'
 itens_path = './FlaskWebProject1/sources/itens.json'
 
+# Get Events
 f = open(events_path, 'r')
-events_file = []
+events = []
 events_file = f.read()
-events = json.loads(events_file)
+events_j = json.loads(events_file)
+for event in events_j:
+    events.append(event)
 f.close()
+
+# Get Itens
 f = open(itens_path, 'r')
-itens_file = []
+itens = []
 itens_file = f.read()
-itens = json.loads(itens_file)
+itens_j = json.loads(itens_file)
+for it in itens_j:
+    itens.append(it)
 f.close()
-
-
 
 @app.route('/')
 @app.route('/home')
 def home():
     return 'Hello World'
 
-@app.route('/teste')
-def teste():
-    return 'teste working'
 
-
-@app.route('/api/herdeiros/eventos/all')
 @app.route('/api/herdeiros/events/all')
 def get_all_events():
     return jsonify(events)
@@ -48,22 +48,22 @@ def create_item(name, description):
     item = {}
     item['name'] = name
     item['description'] = description
-    item_j = json.dumps(item)
-    itens.append(item_j)
+    itens.append(item)
+    itens_file = json.dumps(itens, indent=2)
     f = open(itens_path, 'w+')
-    f.write(item_j)
+    f.write(str(itens_file))
     f.close()
     return 'Item created with success!'
 
 
-@app.route('/api/herdeiros/eventos/create/<name>/<date>')
+@app.route('/api/herdeiros/event/create/<name>/<date>')
 def create_event(name, date):
     evento = {}
     evento['name'] = name
     evento['date'] = date
-    evento_j = json.dumps(evento)
-    events.append(evento_j)
+    events.append(evento)
+    events_file = json.dumps(events, indent=2)
     f = open(events_path, 'w+')
-    f.write(evento_j)
+    f.write(str(events_file))
     f.close()
     return 'Event created with success!'
